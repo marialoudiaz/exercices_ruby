a4d8052e11c4e10d7223ac58972af8e6a613fa3b
marialoudiaz <loumariadiaz@gmail.com>
2023-01-16 11:54:35 +0100

    ADDED MORE PRECISE SALUTING PROG

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, quel est votre Prénom ?"
+print ">"
+user_name1 = gets.chomp
+puts "nice !"
+puts "Quel est votre nom de famille ?"
+print ">"
+user_name = gets.chomp
+puts "Bonjour : #{user_name1} #{user_name}"
+
